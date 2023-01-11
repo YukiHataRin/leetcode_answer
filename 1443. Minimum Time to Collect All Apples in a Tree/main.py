c8ef4dc0c930a1777939bcac19071323b45114e8
YukiHataRin <49299358+YukiHataRin@users.noreply.github.com>
2023-01-11 23:18:44 +0800

    Update main.py

--- 1443. Minimum Time to Collect All Apples in a Tree/main.py
+++ 1443. Minimum Time to Collect All Apples in a Tree/main.py
@@ -11,9 +11,9 @@ class Solution:
 
             for child_node in graph[node]:
                 if child_node != src:
-                    res += dfs(child_node, node)
+                    res += dfs(child_node, node) #往子節點遞迴
 
-            if (res or hasApple[node]) and src != -1:
+            if (res or hasApple[node]) and src != -1: #遇到蘋果則可以開始進入步數，且可以帶著蘋果走回去原點
                 return res + 2
 
             return res
